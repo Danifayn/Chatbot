9e8eb484e1de71ab77fdd68f5fa96c94a9a20368
Karish <danifayn@gmail.com>
2018-06-23 14:54:53 +0300

    Determining insert

--- ChatBot.py
+++ ChatBot.py
@@ -40,12 +40,34 @@ def find_parent(pid):
         return False
 
 
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return  False
+    elif len(data) >1000:
+        return False
+    elif data == '[deleted]' or data = '[removed]':
+        return False
+
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result is not None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print("find parent ", str(e))
+        return False
+
+
 def main():
     create_table()
     row_counter = 0  # How much rows were iterated
     paired_rows = 0  # How many <parent,child> are there
 
-    with open(DATA_DIR + "RC_{}".format(timeframe)) as file:
+    with open(DATA_DIR + "RC_{}".format(timeframe), buffering=1000) as f:
         print("Reading file...")
         for row in f:
             row_counter += 1
@@ -56,6 +78,11 @@ def main():
             score = row['score']
             comment_id = row['name']
             subreddit = row['subreddit']
+            parent_data = find_parent(parent_id)
+
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id)
+                if score > existing_comment_score:
 
 
 if __name__ == '__main__':
